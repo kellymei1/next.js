ec06fc4d24f18ca33124e694a2c44075c6a18676
Tim Neutkens <tim@timneutkens.nl>
2025-08-26 16:57:30 +0200

    Update next-server.ts

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -368,7 +368,6 @@ export default class NextNodeServer extends BaseServer<
           isAppPath: false,
           isDev: this.isDev,
           sriEnabled: this.sriEnabled,
-          needsManifestsForLegacyReasons: false,
         })
       } catch (_err) {
         // Intentionally ignored because this is a preload step.
@@ -383,7 +382,6 @@ export default class NextNodeServer extends BaseServer<
           isAppPath: true,
           isDev: this.isDev,
           sriEnabled: this.sriEnabled,
-          needsManifestsForLegacyReasons: false,
         })
         // we need to ensure fetch is patched before we require the page,
         // otherwise if the fetch is patched by user code, we will be patching it
@@ -894,7 +892,6 @@ export default class NextNodeServer extends BaseServer<
           isAppPath,
           isDev: this.isDev,
           sriEnabled: this.sriEnabled,
-          needsManifestsForLegacyReasons: false,
         })
 
         if (
